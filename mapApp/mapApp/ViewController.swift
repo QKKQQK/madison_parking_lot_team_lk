e93feae6002c785b1dc64f811b516bbb62bfc9bd
QKKQQK <zhouqiankang@hotmail.com>
2017-08-27 23:59:24 -0500

    Zoom to madison

--- mapApp/mapApp/ViewController.swift
+++ mapApp/mapApp/ViewController.swift
@@ -11,9 +11,19 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let location = CLLocationCoordinate2D(latitude: 43.0731,
+                                              longitude: -89.4012)
+        
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        myMap.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
