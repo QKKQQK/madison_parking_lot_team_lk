e38b81cdd846926822b392302347aade6b322758
QKKQQK <zhouqiankang@hotmail.com>
2017-08-28 23:48:59 -0500

    Get user location

--- mapApp/mapApp/ViewController.swift
+++ mapApp/mapApp/ViewController.swift
@@ -9,28 +9,44 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,  CLLocationManagerDelegate{
+    
     @IBOutlet weak var myMap: MKMapView!
+    var locationManager: CLLocationManager?
+    var currentLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let location = CLLocationCoordinate2D(latitude: 43.0731,
-                                              longitude: -89.4012)
-        
-        // 2
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationPermissin()
+        let center = currentLocation?.coordinate
+        let span = MKCoordinateSpanMake(0.02, 0.02)
+        let altLocation = CLLocationCoordinate2D(latitude: 43.0731,
+                                                 longitude: -89.4012)
+        let region = MKCoordinateRegion(center: center ?? altLocation, span: span)
         myMap.setRegion(region, animated: true)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func locationPermissin() {
+        locationManager?.requestAlwaysAuthorization()
+        
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager?.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let currLocation = locations.first {
+            currentLocation = currLocation
+        }
+    }
 }
+
 
